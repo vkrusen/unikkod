a08d52250c56a94d13402f93cd5f1214738bbc44
Victor K <victorkrusenstrahle@gmail.com>
2023-02-09 16:49:27 +0100

    Working on start

---
+++ src/components/start/case.jsx
@@ -0,0 +1,43 @@
+import * as React from "react"
+
+import { Divider } from "../unikkod-components"
+
+import Comfornette from "../../images/Comfornette.jpg"
+
+export default function Case() {
+  return (
+    <div className="container mx-auto py-14 px-3 md:px-0">
+      <div>
+        <SubHeadline />
+        <h2>Hur vi hjälpte Comfornette</h2>
+        <Divider />
+        <ScreenshotCard screenshot={Comfornette} alt="Comfornette" />
+      </div>
+    </div>
+  )
+}
+
+function SubHeadline() {
+  return (
+    <div className="bg-primary rounded-full px-2 py-0 w-fit my-4">
+      <p className="text-white">Kundcase</p>
+    </div>
+  )
+}
+
+function ScreenshotCard({ screenshot, alt }) {
+  return (
+    <div className="flex flex-col md:flex-row gap-4 h-fit md:h-[620px]">
+      <div className="md:w-1/3">
+        <p>Lorem ipsum</p>
+      </div>
+      <div className="w-full md:w-2/3 static md:absolute md:right-0 md:ml-auto">
+        <img
+          className="border-2 border-gray-300 rounded-lg md:rounded-r-none md:border-r-0 md:object-left md:object-cover h-fit md:h-[580px]"
+          src={screenshot}
+          alt={alt}
+        />
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Hero from "../components/start/hero"
 import Seo from "../components/seo"
 import Layout from "../components/layout"
 import Services from "../components/start/services"
+import Case from "../components/start/case"
 
 const IndexPage = () => (
   <Layout>
     <Hero />
     <Services />
+    <Case />
   </Layout>
 )
 
